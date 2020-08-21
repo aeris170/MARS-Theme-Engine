464e85fd06ea2abe8506c53889722ba65291ffef
Doğa Oruç <aeris170@gmail.com>
2020-08-21 11:32:01 +0300

    Update version info

--- mars/Globals.java
+++ mars/Globals.java
@@ -92,7 +92,7 @@ public class Globals {
 	 * The current MARS version number. Can't wait for "initialize()" call to get
 	 * it.
 	 */
-	public static final String version = "4.5";
+	public static final String version = "4.5.1 Unofficial";
 	/** List of accepted file extensions for MIPS assembly source files. */
 	public static final ArrayList fileExtensions = getFileExtensions();
 	/** Maximum length of scrolled message window (MARS Messages and Run I/O) */
